518288865a19672134ea43d2a7baa3e67d09152c
Marco Scarciofolo <scar_marco@hotmail.es>
2021-10-20 00:33:53 +0200

    nav changes

--- components/Nav.js
+++ components/Nav.js
@@ -5,7 +5,9 @@ import { acts } from "../constants";
 const Nav = () => {
   return (
     <nav className="flex px-6 py-2 text-lg bg-gray-900 shadow-sm text-gray-300 mb-6">
-      <h1 className="flex-1 font-semibold">Path of Exile Leveling Guide</h1>
+      <Link href="/">
+        <a className="flex-1 font-semibold">Path of Exile Leveling Guide</a>
+      </Link>
       <div className="flex justify-between flex-1 ml-4">
         {acts.map((act) => (
           <Link
